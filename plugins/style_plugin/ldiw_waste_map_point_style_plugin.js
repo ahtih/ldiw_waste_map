b888607cdc59bb040dc799963b03e6582f5f138e
Ahti Heinla <ahti@ahti.bluemoon.ee>
2010-08-24 10:38:41 +0300

    implemented custom map popup showing diameter, nr_of_tires, body, nr_of_nodes

--- plugins/style_plugin/ldiw_waste_map_point_style_plugin.js
+++ plugins/style_plugin/ldiw_waste_map_point_style_plugin.js
@@ -18,3 +18,26 @@ Drupal.openlayers.styleContext.ldiw_waste_map_context=function(parameters) {
 													'#d07070' : '#ff4040';
 			};
 	};
+
+Drupal.theme.openlayersPopup=function(feature)
+{
+	var output='';
+	if (feature.attributes.nr_of_nodes == '') {
+		output='Diameter ' + feature.attributes.field_diameter_value +
+																	'm<br>';
+		if (feature.attributes.field_nr_of_tires_value != '' &&
+					feature.attributes.field_nr_of_tires_value != '0') {
+			output+='Contains ' +
+							feature.attributes.field_nr_of_tires_value +
+							' tires<br>';
+			}
+		if (feature.attributes.body != '') {
+			output+='<br>' + feature.attributes.body;
+			}
+		}
+	else {
+		output=feature.attributes.nr_of_nodes + ' waste points';
+		}
+
+	return '<div class="openlayers-popup">' + output + '</div>';
+	}
