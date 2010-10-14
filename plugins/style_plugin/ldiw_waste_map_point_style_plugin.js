0b9f470702ad5e6e072f6c44840581bc0ebcf291
Ahti Heinla <ahti@ahti.bluemoon.ee>
2010-10-14 23:53:33 +0300

    Added composition percentage fields to Waste point; also added them to Geoclustering tree, and Waste point map popup

--- plugins/style_plugin/ldiw_waste_map_point_style_plugin.js
+++ plugins/style_plugin/ldiw_waste_map_point_style_plugin.js
@@ -26,26 +26,48 @@ Drupal.theme.openlayersPopup=function(feature)
 	var coeff=attrs.field_volume_value >= 5 ? 1 : 10;
 	var volume_formatted=Math.round(attrs.field_volume_value * coeff) / coeff;
 
+	var composition_map={};
+	var composition_sum=0;
+	for (var attrname in attrs) {
+		var type=attrname.replace(/^field_composition_(.*)_value/,'$1');
+		if (type != attrname && attrs[attrname]) {
+			var value=parseFloat(attrs[attrname]);
+			if (value > 0) {
+				composition_map[type]=value;
+				composition_sum+=value;
+				}
+			}
+		}
+
+	var composition_array=new Array();
+	for (var type in composition_map) {
+		composition_array.push(Math.max(1,
+				parseInt(composition_map[type] * 100 / composition_sum)) +
+				'% ' + type);
+		}
+
+	if (attrs.field_nr_of_tires_value &&
+								attrs.field_nr_of_tires_value != '0') {
+		composition_array.push(attrs.field_nr_of_tires_value + ' tires');
+		}
+
+	var composition=composition_array.join(', ');
+	if (composition) {
+		composition='<br>Composition: ' + composition;
+		}
+
 	var output='';
 	if (attrs.nr_of_nodes && attrs.nr_of_nodes > 1) {
 		output=attrs.nr_of_nodes + ' waste points<br>' + 
-				'Total volume ' + volume_formatted + 'm&sup3;';
-		if (attrs.field_nr_of_tires_value != '' &&
-								attrs.field_nr_of_tires_value != '0') {
-			output+='<br>Contains ' + attrs.field_nr_of_tires_value +
-																' tires';
-			}
+				'Total volume ' + volume_formatted + 'm&sup3;' +
+				composition;
 		}
 	else {
 		output='Volume ' + volume_formatted + 'm&sup3;';
-		if (attrs.field_diameter_value != '') {
+		if (attrs.field_diameter_value) {
 			output+='<br>Diameter ' + attrs.field_diameter_value + 'm';
 			}
-		if (attrs.field_nr_of_tires_value != '' &&
-								attrs.field_nr_of_tires_value != '0') {
-			output+='<br>Contains ' + attrs.field_nr_of_tires_value +
-																' tires';
-			}
+		output+=composition;
 		if (attrs.body != '') {
 			output+='<br><br>' + attrs.body;
 			}
