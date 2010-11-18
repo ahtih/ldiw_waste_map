de11758a8b2d7bf8394bff4f7174b787ca66439e
Ahti Heinla <ahti@ahti.bluemoon.ee>
2010-11-18 10:32:26 +0200

    Fixed a bug that messed up OpenLayers popups in maps other than ours

--- plugins/style_plugin/ldiw_waste_map_point_style_plugin.js
+++ plugins/style_plugin/ldiw_waste_map_point_style_plugin.js
@@ -1,7 +1,59 @@
 // $Id$
 
 Drupal.openlayers.style_plugin.ldiw_waste_map_point_style_plugin=
-													function(parameters) {}
+													function(parameters) {
+Drupal.theme.openlayersPopup=function(feature)
+{
+	var attrs=feature.attributes;
+
+	var coeff=attrs.field_volume_value >= 5 ? 1 : 10;
+	var volume_formatted=Math.round(attrs.field_volume_value * coeff) / coeff;
+
+	var composition_map={};
+	var composition_sum=0;
+	for (var attrname in attrs) {
+		var type=attrname.replace(/^field_composition_(.*)_value/,'$1');
+		if (type != attrname && attrs[attrname]) {
+			var value=parseFloat(attrs[attrname]);
+			if (value > 0) {
+				composition_map[type]=value;
+				composition_sum+=value;
+				}
+			}
+		}
+
+	var composition_array=[];
+	for (var type in composition_map)
+		composition_array.push(Math.max(1,
+				parseInt(composition_map[type] * 100 / composition_sum)) +
+				'% ' + type);
+
+	if (attrs.field_nr_of_tires_value &&
+								attrs.field_nr_of_tires_value != '0')
+		composition_array.push(attrs.field_nr_of_tires_value + ' tires');
+
+	var composition=composition_array.join(', ');
+	if (composition)
+		composition='<br>Composition: ' + composition;
+
+	var output='';
+	if (attrs.nr_of_nodes && attrs.nr_of_nodes > 1) {
+		output=attrs.nr_of_nodes + ' waste points<br>' + 
+				'Total volume ' + volume_formatted + 'm&sup3;' +
+				composition;
+		}
+	else {
+		output='Volume ' + volume_formatted + 'm&sup3;';
+		if (attrs.field_diameter_value)
+			output+='<br>Diameter ' + attrs.field_diameter_value + 'm';
+		output+=composition;
+		if (attrs.body != '')
+			output+='<br><br>' + attrs.body;
+		}
+
+	return '<div class="openlayers-popup">' + output + '</div>';
+	}
+}
 
 Drupal.openlayers.style_plugin.ldiw_waste_map_point_style_plugin.prototype=
 {
@@ -95,55 +147,3 @@ Drupal.openlayers.style_plugin.ldiw_waste_map_point_style_plugin.prototype=
 			return '#' + composition[0][0];
 			},
 	};
-
-Drupal.theme.openlayersPopup=function(feature)
-{
-	var attrs=feature.attributes;
-
-	var coeff=attrs.field_volume_value >= 5 ? 1 : 10;
-	var volume_formatted=Math.round(attrs.field_volume_value * coeff) / coeff;
-
-	var composition_map={};
-	var composition_sum=0;
-	for (var attrname in attrs) {
-		var type=attrname.replace(/^field_composition_(.*)_value/,'$1');
-		if (type != attrname && attrs[attrname]) {
-			var value=parseFloat(attrs[attrname]);
-			if (value > 0) {
-				composition_map[type]=value;
-				composition_sum+=value;
-				}
-			}
-		}
-
-	var composition_array=[];
-	for (var type in composition_map)
-		composition_array.push(Math.max(1,
-				parseInt(composition_map[type] * 100 / composition_sum)) +
-				'% ' + type);
-
-	if (attrs.field_nr_of_tires_value &&
-								attrs.field_nr_of_tires_value != '0')
-		composition_array.push(attrs.field_nr_of_tires_value + ' tires');
-
-	var composition=composition_array.join(', ');
-	if (composition)
-		composition='<br>Composition: ' + composition;
-
-	var output='';
-	if (attrs.nr_of_nodes && attrs.nr_of_nodes > 1) {
-		output=attrs.nr_of_nodes + ' waste points<br>' + 
-				'Total volume ' + volume_formatted + 'm&sup3;' +
-				composition;
-		}
-	else {
-		output='Volume ' + volume_formatted + 'm&sup3;';
-		if (attrs.field_diameter_value)
-			output+='<br>Diameter ' + attrs.field_diameter_value + 'm';
-		output+=composition;
-		if (attrs.body != '')
-			output+='<br><br>' + attrs.body;
-		}
-
-	return '<div class="openlayers-popup">' + output + '</div>';
-	}
