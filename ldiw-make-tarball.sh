1f8c651544e7d043cb1055c68fc2f631968ceade
Ahti Heinla <ahti@ahti.bluemoon.ee>
2011-03-05 10:31:51 +0200

    Added shell script to make a tarball for simple installation of LDIW Waste Map

---
+++ ldiw-make-tarball.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+TMPDIR=/tmp/ldiw-make-tarball-tmpdir
+
+get_from_git() {
+	git clone $2 $1 && rm -rf $1/.git
+	}
+
+rm -rf $TMPDIR &&
+mkdir -p $TMPDIR &&
+
+pushd $TMPDIR &&
+
+get_from_git 'geoclustering' 'git://github.com/ahtih/Geoclustering.git' &&
+get_from_git 'ldiw_waste_map' 'git://github.com/ahtih/ldiw_waste_map.git' &&
+get_from_git 'geo' 'git@git.drupal.org:sandbox/ahtih/1081362.git' &&
+get_from_git 'views_bonus' 'git@git.drupal.org:sandbox/ahtih/1081884.git' &&
+
+(curl http://ftp.drupal.org/files/projects/services-6.x-3.x-dev.tar.gz | tar xz) &&
+(curl http://ftp.drupal.org/files/projects/openlayers-6.x-2.x-dev.tar.gz | tar xz) &&
+
+fnames=`ls -1` &&
+popd &&
+tar -czf $1 -C $TMPDIR $fnames &&
+rm -rf $TMPDIR
